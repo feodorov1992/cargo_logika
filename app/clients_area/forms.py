7c8d48ddb6f43cffbfc7d359adca26fe342fe02c
DarthMole <feodorov1992@mail.ru>
2023-09-26 22:03:43 +0300

    Order filter updated with orders delivery date criteria

--- app/clients_area/forms.py
+++ app/clients_area/forms.py
@@ -8,3 +8,13 @@ class ClientsAreaOrdersFilters(forms.Form):
                                       widget=forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'))
     order_date__lte = forms.DateField(label=_('Not later'), required=False,
                                       widget=forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'))
+    delivery_date__gte = forms.DateField(label=_('Not earlier'), required=False,
+                                         widget=forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'))
+    delivery_date__lte = forms.DateField(label=_('Not later'), required=False,
+                                         widget=forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'))
+
+    def as_my_style(self):
+        context = super().get_context()
+        context['fields'] = {f_e[0].name: f_e[0] for f_e in context['fields']}
+        context['hidden_fields'] = {f_e.name: f_e for f_e in context['hidden_fields']}
+        return self.render(template_name='clients_area/basic_styles/order_filters_as_my_style.html', context=context)
